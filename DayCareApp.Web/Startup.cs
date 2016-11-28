fee404d08756fb4887e6c183d512520f9503b204
Nephross <Nephross@users.noreply.github.com>
2016-11-28 13:56:59 +0100

    adding ident framework

---
+++ DayCareApp.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DayCareApp.Web.Startup))]
+namespace DayCareApp.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
