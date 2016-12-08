e1ca3ba921db5c2f2c29638aa485d56af0228b4a
Nephross <Nephross@users.noreply.github.com>
2016-12-08 13:24:01 +0100

    Adding dummy accounts

--- DayCareApp.Web/DataContext/Migrations/Configuration.cs
+++ DayCareApp.Web/DataContext/Migrations/Configuration.cs
@@ -44,6 +44,54 @@ namespace DayCareApp.Web.DataContext.Migrations
             userManager.AddToRole(client1.Id, "Admin");
 
 
+
+            var client2 = new ApplicationUser { UserName = "institutionAdmin@daycareapp.dk" };
+
+            //Client to usermngr with password
+            var result2 = userManager.Create(client2, "password");
+
+            //If the user already exist in the usrmngr we aquire it for later use.
+            if (result2.Succeeded == false)
+            {
+                client2 = userManager.FindByName("institutionAdmin@daycareapp.dk");
+            }
+
+            //Add the new user to admin role
+            userManager.AddToRole(client2.Id, "InstitutionAdmin");
+
+
+        
+
+            var client3 = new ApplicationUser { UserName = "testEmployee@daycareapp.dk" };
+
+            //Client to usermngr with password
+            var result3 = userManager.Create(client3, "password");
+
+            //If the user already exist in the usrmngr we aquire it for later use.
+            if (result3.Succeeded == false)
+            {
+                client3 = userManager.FindByName("testEmployee@daycareapp.dk");
+            }
+
+            //Add the new user to admin role
+            userManager.AddToRole(client3.Id, "Employee");
+
+
+
+            var client4 = new ApplicationUser { UserName = "testParent@daycareapp.dk" };
+
+            //Client to usermngr with password
+            var result4 = userManager.Create(client4, "password");
+
+            //If the user already exist in the usrmngr we aquire it for later use.
+            if (result4.Succeeded == false)
+            {
+                client4 = userManager.FindByName("testParent@daycareapp.dk");
+            }
+
+            //Add the new user to admin role
+            userManager.AddToRole(client4.Id, "Parent");
+
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data. E.g.
             //
